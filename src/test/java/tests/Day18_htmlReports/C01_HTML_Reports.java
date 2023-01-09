8396ca5bad1113a8006a026deb95e1054b13044f
HasanTASAR <115658750+HasanTASAR@users.noreply.github.com>
2023-01-09 21:07:47 +0300

    9 ocak

---
+++ src/test/java/tests/Day18_htmlReports/C01_HTML_Reports.java
@@ -0,0 +1,14 @@
+package tests.Day18_htmlReports;
+
+import org.testng.annotations.Test;
+import utilities.TestBaseRapor;
+
+public class C01_HTML_Reports extends TestBaseRapor {
+    @Test
+    public void test01(){
+
+
+
+
+    }
+}
